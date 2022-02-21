02854bf80fed0ca493a026490787fc7086ab9aa6
Fani Jasmakovska <fani.jasmakovska@gmail.com>
2022-02-21 13:57:17 +0100

    min deployment target to be iOS 11.

--- Sources/Views/Controller.swift
+++ Sources/Views/Controller.swift
@@ -215,8 +215,10 @@ public class FastisController<Value: FastisValue>: UIViewController, JTACMonthVi
         let navVc = UINavigationController(rootViewController: self)
         navVc.modalTransitionStyle = viewController.modalTransitionStyle
         navVc.transitioningDelegate = viewController.transitioningDelegate
-        navVc.isModalInPresentation = viewController.isModalInPresentation
-        navVc.modalPresentationStyle = .formSheet
+        if #available(iOS 13.0, *) {
+            navVc.isModalInPresentation = viewController.isModalInPresentation
+            navVc.modalPresentationStyle = .formSheet
+        }
         if viewController.preferredContentSize != .zero {
             navVc.preferredContentSize = viewController.preferredContentSize
         } else {
@@ -232,9 +234,11 @@ public class FastisController<Value: FastisValue>: UIViewController, JTACMonthVi
         self.view.backgroundColor = self.appearance.backgroundColor
         self.navigationController?.navigationBar.titleTextAttributes = self.appearance.titleTextAttributes
 
-        let appearnce = UINavigationBarAppearance()
-        appearnce.configureWithTransparentBackground()
-        self.navigationItem.standardAppearance = appearnce
+        if #available(iOS 13.0, *) {
+            let appearnce = UINavigationBarAppearance()
+            appearnce.configureWithTransparentBackground()
+            self.navigationItem.standardAppearance = appearnce
+        }
         self.navigationItem.largeTitleDisplayMode = .never
         self.navigationItem.leftBarButtonItem = self.cancelBarButtonItem
         self.navigationItem.rightBarButtonItem = self.doneBarButtonItem
